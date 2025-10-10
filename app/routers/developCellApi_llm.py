c99274ee51c72f7ab40f760a61d8d13278836828
seokhwan <opiiipoo@gmail.com>
2025-10-10 14:12:52 +0900

    merge

--- app/routers/developCellApi_llm.py
+++ app/routers/developCellApi_llm.py
@@ -5,6 +5,8 @@ from app.services import developCellApi_service
 router = APIRouter()
 
 
-@router.post("/chat/completions")
+@router.post("/chat/completions",
+             summary="llm 질의응답"
+             )
 async def get_chat_completion(request: ChatCompletionRequest):
     return await developCellApi_service.get_chat_completion_from_lms(request)
